f3a44a3254f3675b4a9b7a8963e910488fa781c9
cetaceanw <jing-yu.wang@outlook.com>
2022-07-30 17:00:44 +0200

    Rendered a sphere

--- vec3.h
+++ vec3.h
@@ -53,4 +53,57 @@ class vec3 {
 typedef vec3 point3; // 3D point
 typedef vec3 color; // RGB color
 
+// vec3 utility functions
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v)
+{
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t)
+{
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v)
+{
+    return v * t;
+}
+
+inline vec3 operator/(const vec3 &v, double t)
+{
+    return (1/t) * v;
+}
+
+inline double dot(const vec3 &u, const vec3 &v)
+{
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(const vec3 &v)
+{
+    return v / v.length();
+}
+
 #endif
