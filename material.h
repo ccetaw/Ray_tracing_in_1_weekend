86f98dbf229a60443cfdcda527a7075c4fa829df
cetaceanw <jing-yu.wang@outlook.com>
2023-01-05 14:53:37 +0100

    Finished all

--- material.h
+++ material.h
@@ -44,9 +44,10 @@ class metal : public material
 {
 public:
     color albedo;
+    double fuzz;
 
 public:
-    metal(const color& a): albedo(a) {}
+    metal(const color& a, double f): albedo(a), fuzz(f < 1 ? f : 1) {}
     virtual bool scatter(
         const ray& r_in, 
         const hit_record& rec, 
@@ -54,7 +55,7 @@ public:
         ray& scattered) const override
     {
         auto reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
         attenuation = albedo;
         return (dot(scattered.direction(), rec.normal) > 0);
     }
